ef5cd64f9f7944ae9bf66815354b35af48bdf34e
LordMero <graziano.mirata@gmail.com>
2020-12-17 23:34:58 +0000

    begin second part of day 4

---
+++ 2020/04/solution.py
@@ -0,0 +1,12 @@
+from ../libs import *
+
+val[]
+
+
+val['byr'] = (1920, 2002) 
+val['iyr'] = (2010, 2020)
+val['eyr'] = (4, 2020, 2030)
+val['hgt'] = [(cm, 150,  193), (in,  59, 76)]
+val['hcl'] =  #  0-9 or a-f.
+val['ecl'] =  amb blu brn gry grn hzl oth.
+val['pid'] = nine-digit number, including leading zeroes.
