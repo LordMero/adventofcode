76bc5e518dadf7f2ba2be0d243116b7e609ed5ab
LordMero <graziano.mirata@gmail.com>
2020-12-18 09:35:55 +0000

    continue second part

--- 2020/04/solution.py
+++ 2020/04/solution.py
@@ -8,5 +8,22 @@ val['iyr'] = (2010, 2020)
 val['eyr'] = (4, 2020, 2030)
 val['hgt'] = [(cm, 150,  193), (in,  59, 76)]
 val['hcl'] =  #  0-9 or a-f.
-val['ecl'] =  amb blu brn gry grn hzl oth.
-val['pid'] = nine-digit number, including leading zeroes.
+val['ecl'] = ['amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth'] 
+val['pid'] = nine-digit number, including leading zeroes. 
+
+
+def validate_fields(fld):
+    if fld == 'byr':
+        if 
+    elif fld == 'iyr':
+    
+    elif fld == 'eyr':
+
+    elif fld == 'hgt':
+
+    elif fld == 'hcl':
+
+    elif fld == 'ecl':
+
+    elif fld == 'pid':
+
